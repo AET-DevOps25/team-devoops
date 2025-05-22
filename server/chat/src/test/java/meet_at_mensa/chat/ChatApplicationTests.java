5e8e6672c1e07a6af01b383cc13b0039881e0c65
Anastasiia Korzhylova <nastya.korzhylova@gmail.com>
2025-05-22 13:34:24 +0200

    Add matching service

--- server/chat/src/test/java/meet_at_mensa/chat/ChatApplicationTests.java
+++ server/chat/src/test/java/meet_at_mensa/chat/ChatApplicationTests.java
@@ -1,10 +1,11 @@
-package meet_at_mensa.chat_service;
+package meet_at_mensa.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChatServiceApplicationTests {
+// ChatApplicationTests is a test class for the chat application
+class ChatApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ server/matching/src/test/java/meet_at_mensa/matching/MatchingApplicationTests.java
@@ -0,0 +1,14 @@
+package meet_at_mensa.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+// MatchingApplicationTests is a test class for the matching application
+class MatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
