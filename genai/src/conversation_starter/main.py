4c96b887bde1b333371982214a78ff86576cf3b3
Guilherme Stark <guilhermestark@protonmail.com>
2025-05-22 18:17:45 +0200

    feat: implemented basic webserver as a placeholder

--- genai/src/conversation_starter/main.py
+++ genai/src/conversation_starter/main.py
@@ -1,2 +1,20 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# Port for the webserver
+PORT = 80
+
+# Basic webserver will return "Hello World!" when accessed
+class Webserver(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello World!")
+
+# Main function available via meetatmensa-start-genai script
 def start_genai():
-    print("hello world")
+
+    # Start http server and serve until stopped
+    with HTTPServer(("", PORT), Webserver) as httpd:
+        print(f"Serving on port {PORT}")
+        httpd.serve_forever()
