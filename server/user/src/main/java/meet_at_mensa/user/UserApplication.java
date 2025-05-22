f8ab2ad3597b25c16dce50acb8417afec29ba896
Anastasiia Korzhylova <nastya.korzhylova@gmail.com>
2025-05-22 13:52:01 +0200

    Add user service

---
+++ server/user/src/main/java/meet_at_mensa/user/UserApplication.java
@@ -0,0 +1,27 @@
+package meet_at_mensa.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+// UserApplication is the main class for the user application
+public class UserApplication {
+
+    // The main method is the entry point of the user application
+    // It starts the Spring Boot application
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@RestController
+    // HelloController is a REST controller that handles HTTP GET requests
+    // It returns a greeting message in plain text
+    class HelloController {
+        @GetMapping("/")
+        public String hello() {
+            return "Hello World! Welcome to the User Service!";
+        }
+    }
+}
---
+++ server/user/src/test/java/meet_at_mensa/user/UserApplicationTests.java
@@ -0,0 +1,14 @@
+package meet_at_mensa.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+// UserApplicationTests is a test class for the user application
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
