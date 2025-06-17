6692f92adc4ba4dc9b4e843a6c19ce4dbf66dbc8
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-17 17:35:03 +0200

    feat: add basic implementation for chatdb and matchdb

---
+++ database/chatdb/init_chatdb.sql
@@ -0,0 +1,21 @@
+-- Create user-data table
+-- chat_id: UUID of the chat
+-- group_id: UUID of the group in the chat
+CREATE TABLE IF NOT EXISTS chats (
+    chat_id INT PRIMARY KEY,
+    group_id INT
+);
+
+-- Create user-credentials table
+-- message_id: UUID of the message
+-- chat_id: UUID of the chat the message was sent in
+-- user_id: UUID of the author of the message
+-- timestamp: time the message was sent
+-- content: content of the message
+CREATE TABLE IF NOT EXISTS messages (
+    message_id INT PRIMARY KEY,
+    chat_id INT,
+    user_id INT,
+    timestamp INT,
+    content VARCHAR(255) NOT NULL
+);
