7275572c1fd1bd7a91b27e12e971af515b831af9
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-17 17:34:39 +0200

    docs: updated comments for userdb

--- database/userdb/init_userdb.sql
+++ database/userdb/init_userdb.sql
@@ -1,4 +1,8 @@
 -- Create user-data table
+-- user_id: UUID of the given user
+-- name: user's name
+-- gender: user's gender
+-- TODO: Add more data
 CREATE TABLE IF NOT EXISTS user_data (
     user_id INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -6,6 +10,9 @@ CREATE TABLE IF NOT EXISTS user_data (
 );
 
 -- Create user-credentials table
+-- user_id: UUID of the given user
+-- email: user's email used on login
+-- password_hash: user's hashed password
 CREATE TABLE IF NOT EXISTS user_credentials (
     user_id INT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
