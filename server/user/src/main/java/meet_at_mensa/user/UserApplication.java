b704e60b9966121b91085e51fdf8c26ab3419202
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-18 17:43:41 +0200

    feat: added wait for db startup to user-service

--- server/user/src/main/java/meet_at_mensa/user/UserApplication.java
+++ server/user/src/main/java/meet_at_mensa/user/UserApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 // UserApplication is the main class for the user application
@@ -12,8 +13,30 @@ public class UserApplication {
     // The main method is the entry point of the user service
     // It starts the Spring Boot application
 	public static void main(String[] args) {
+
+        wait_for_database_startup();
+
 		SpringApplication.run(UserApplication.class, args);
 	}
+
+    // Wait 20s to ensure that databases have started
+    private static void wait_for_database_startup() {
+
+        System.out.println("Waiting for databases to start...");
+
+        try {
+
+            TimeUnit.SECONDS.sleep(20);
+
+        } catch (InterruptedException e) {
+
+            Thread.currentThread().interrupt();
+            System.out.println("Wait was interrupted");
+            
+        }
+
+        System.out.println("Done waiting for databases to start");        
+    }
 
 	@RestController
     // HelloController is a REST controller that handles HTTP GET requests
