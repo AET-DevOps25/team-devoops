43771f26a13fa558df42540e1e335b2a161f15f4
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-21 13:17:16 +0200

    feat: add functionality to update existing users

--- server/user/src/main/java/meet_at_mensa/user/UserController.java
+++ server/user/src/main/java/meet_at_mensa/user/UserController.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+import java.util.UUID;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -15,16 +21,58 @@ public class UserController {
     @Autowired // This is auto-implemented by spring
     private UserRepository userRepository;
 
-    // Responds to get requests on /users/all with a list of all users
+    // Show all users in database
+    // TODO: Remove in production
     @GetMapping(path="/debug/users/all")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     // Create a new user when provided with json formatted data
-    @PostMapping(path="/api/users/new")
+    @PostMapping(path="/api/users/create")
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    // Return user information
+    @GetMapping(path="/api/users/get/{userID}")
+    public User getUser(@PathVariable UUID userID) {
+        return userRepository.findById(userID)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    // Update user information
+    @PostMapping(path="api/users/edit/{userID}")
+    public User editUser(@PathVariable UUID userID, @RequestBody Map<String, String> update) {
+        
+        // find user
+        User user = userRepository.findById(userID)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        // Update values if present in payload
+        if (update.containsKey("email")) {
+            user.setEmail(update.get("email"));
+        }
+
+        if (update.containsKey("name")) {
+            user.setName(update.get("name"));
+        }
+
+        if (update.containsKey("gender")) {
+            user.setGender(update.get("gender"));
+        }
+
+        if (update.containsKey("degree")) {
+            user.setDegree(update.get("degree"));
+        }
+
+        if (update.containsKey("birthday")) {
+            // implement later
+        }
+
+        // Save to Database and Return
+        return userRepository.save(user);
+
     }
 
 }
