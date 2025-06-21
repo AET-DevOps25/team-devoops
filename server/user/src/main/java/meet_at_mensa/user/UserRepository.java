948fbc3be926589590e3ca75d04377e8e0633650
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-21 13:16:00 +0200

    fix: corrected repository to accurately reflect UUID as ID

--- server/user/src/main/java/meet_at_mensa/user/UserRepository.java
+++ server/user/src/main/java/meet_at_mensa/user/UserRepository.java
@@ -2,10 +2,13 @@ package meet_at_mensa.user;
 
 // import CRUD repository (Create/Read/Update/Delete)
 import org.springframework.data.repository.CrudRepository;
+import java.util.UUID;
 
 // Interface UserRepository represents the userdb table
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, UUID> {
     
     // This is auto-implemented by springboot into a userRepository Bean
+
+    // We only ever need to get users by the UUID, so no further functions need to be defined here
 
 }
