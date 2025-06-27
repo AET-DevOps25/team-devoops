ecddeeecd80e2e258194d3a58bf6a63674b22d40
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-27 14:10:42 +0200

    feat: added custom script for generating code and build files

--- api/scripts/gen_code.sh
+++ api/scripts/gen_code.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-#
-openapi-generator-cli generate -g spring -i api/openapi.yaml --additional-properties=useTags=true,buildTool=gradle,interfaceOnly=true -o server/matching/generated
+
+for path in matching user; do
+
+    # echo generating code
+    echo "Generating code for $path..."
+
+    # generate code based on OpenAPI specification
+    openapi-generator-cli generate \
+        -g spring \
+        -i api/openapi.yaml \
+        -o server/{$path}/generated \
+        --additional-properties=useTags=true,interfaceOnly=true
+
+    # echo generating gradle
+    echo "Generating gradle project files for $path..."
+
+    # generate gradle build files.
+    gradle init \
+        --type pom \
+        --dsl groovy \
+        --no-incubating
+        --project-dir server/{$path}/generated
+
+done
