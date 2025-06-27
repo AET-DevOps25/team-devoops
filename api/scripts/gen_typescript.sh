d86e681aac8da53178d3e8d595405ebfe527840c
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-27 19:19:01 +0200

    feat: add standalone lint script

--- api/scripts/gen_typescript.sh
+++ api/scripts/gen_typescript.sh
@@ -7,12 +7,6 @@
 #       - openapi-typescript
 #       - typescript
 
-# fail command if any part fails
-set -euo pipefail
-
-# echo linting
-echo "Linting api/openapi.yaml..."
-
 # lint openapi spec
 redocly lint api/openapi.yaml
 
