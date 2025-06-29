a39d066affc85dd06a5d55aafa964f657ec69d33
Guilherme Stark <guilhermestark@protonmail.com>
2025-06-29 13:58:36 +0200

    feat: Update userdb to match api v2.1

--- server/database/userdb/init_userdb.sql
+++ server/database/userdb/init_userdb.sql
@@ -9,9 +9,11 @@
 CREATE TABLE IF NOT EXISTS `user_data` (
     user_id VARBINARY(16) PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
     gender VARCHAR(255) NOT NULL,
     degree VARCHAR(255) NOT NULL,
+    degree_start INT NOT NULL,
     birthday DATE NOT NULL
 ); 
 
@@ -23,12 +25,4 @@ CREATE TABLE IF NOT EXISTS `user_interests` (
     list_id VARBINARY(16) PRIMARY KEY,
     user_id VARBINARY(16) NOT NULL,
     interest VARCHAR(255) NOT NULL
-);
-
--- user_credentials table
-    -- user_id: UUID of the given user
-    -- password_hash: user's hashed password
-CREATE TABLE IF NOT EXISTS `user_credentials` (
-    user_id VARBINARY(16) PRIMARY KEY,
-    password_hash VARCHAR(255) NOT NULL
 );
