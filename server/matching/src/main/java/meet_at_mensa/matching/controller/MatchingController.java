1a4865642cc6654e44b16d5607151944e03a0ad7
Guilherme Stark <guilhermestark@protonmail.com>
2025-07-01 16:34:27 +0200

    feat: create structure for MatchingController

--- server/matching/src/main/java/meet_at_mensa/matching/controller/MatchingController.java
+++ server/matching/src/main/java/meet_at_mensa/matching/controller/MatchingController.java
@@ -1,24 +1,70 @@
 package meet_at_mensa.matching.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.openapitools.api.MatchingApi;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.server.ResponseStatusException;
-
-import org.springframework.http.HttpStatus;
+import jakarta.validation.Valid;
 
 import java.util.UUID;
-import java.util.Map;
+
+import org.openapitools.api.MatchingApi;
+import org.openapitools.model.*;
 
 @RestController
 public class MatchingController implements MatchingApi {
+
+    // DELETE @ api/v2/matching/request/{requestID}
+    @Override
+    public ResponseEntity<Void> deleteApiV2MatchingRequestRequestId(UUID requestId) {
+        // TODO: Implement deletion logic using requestId
+        return ResponseEntity.ok().build();
+    }
+
+    // GET @ api/v2/matching/matches/{userID}
+    @Override
+    public ResponseEntity<MatchCollection> getApiV2MatchingMatchesUserID(UUID userId) {
+        // TODO: Fetch matches for userId
+        MatchCollection collection = new MatchCollection();
+        return ResponseEntity.ok(collection);
+    }
+
+    // GET @ api/v2/matching/requests/{userID}
+    @Override
+    public ResponseEntity<MatchRequestCollection> getApiV2MatchingRequestsUserID(UUID userId) {
+        // TODO: Fetch match requests for userId
+        MatchRequestCollection collection = new MatchRequestCollection();
+        return ResponseEntity.ok(collection);
+    }
+
+    // GET @ api/v2/matching/rsvp/{matchID}/accept
+    @Override
+    public ResponseEntity<Void> getApiV2MatchingRsvpMatchIdAccept(UUID matchId) {
+        // TODO: Handle RSVP accept
+        return ResponseEntity.ok().build();
+    }
+
+    // GET @ api/v2/matching/rsvp/{matchID}/reject
+    @Override
+    public ResponseEntity<Void> getApiV2MatchingRsvpMatchIdReject(UUID matchId) {
+        // TODO: Handle RSVP reject
+        return ResponseEntity.ok().build();
+    }
+
+    // POST @ api/v2/matching/request/submit
+    @Override
+    public ResponseEntity<Void> postApiV2MatchingRequestSubmit(@Valid MatchRequestNew matchRequestNew) {
+        // TODO: Handle new match request submission
+        return ResponseEntity.ok().build();
+    }
+
+    // PUT @ api/v2/matching/request/{requestID}
+    @Override
+    public ResponseEntity<MatchRequest> putApiV2MatchingRequestRequestId(
+            UUID requestId,
+            @Valid MatchRequestUpdate matchRequestUpdate) {
+        // TODO: Handle updating match request
+        MatchRequest updated = new MatchRequest(); // Fill with updated data
+        return ResponseEntity.ok(updated);
+    }
 
 }
