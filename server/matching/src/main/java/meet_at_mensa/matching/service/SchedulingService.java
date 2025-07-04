e54389d73c18ef1e474ef1eb280b075e79ff5bb6
Guilherme Stark <guilhermestark@protonmail.com>
2025-07-04 09:51:46 +0200

    feat: add skeleton for scheduling service

--- server/matching/src/main/java/meet_at_mensa/matching/service/SchedulingService.java
+++ server/matching/src/main/java/meet_at_mensa/matching/service/SchedulingService.java
@@ -1,13 +1,79 @@
 package meet_at_mensa.matching.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import meet_at_mensa.matching.exception.RequestNotFoundException;
 
 @Service
 public class SchedulingService {
-    
-    // Cleanup expired requests
-    // Cleanup expired Groups / matches
-    // Trigger Matching Runs
-    // Trigger Group Health-Checks
+
+
+    /**
+     * Sets the status of expired matches
+     * 
+     * Runs at 15:00, just after the Mensa closes
+     *
+     * This only sets the status to Expired, and does not delete the entries from the tables
+     * 
+     */
+    @Scheduled(cron = "0 0 15 * * *", zone = "Europe/Berlin")
+    public void scheduledExpiry(){
+
+        // TODO: Implement
+
+    }
+
+    /**
+     * Removes any expired entries from the database
+     * 
+     * Runs once a week on Sundays at 00:00
+     *
+     * 
+     */
+    @Scheduled(cron = "0 0 0 * * SUN", zone = "Europe/Berlin")
+    public void scheduledCleanup(){
+
+        // TODO: Implement
+
+    }
+
+
+
+    /**
+     * Runs a scheduled round of matching
+     * 
+     * Runs the day before at 22:00, and the day of at 10:00
+     *
+     *
+     * The 22:00 round is intended to serve as the intended matching run for a given day
+     * with the 10:00 round serving to REMATCH any groups that failed a health-check
+     * 
+     */
+    @Scheduled(cron = "0 0 22,10 * * *", zone = "Europe/Berlin")
+    public void scheduledMatching(){
+
+        // TODO: Implement
+
+    }
+
+
+
+    /**
+     * Checks group RSVP status to see if it's dead and needs to be re-mathched
+     * 
+     * Runs every 10 minutes
+     *
+     *
+     * If a group has enough rejections that it would end up with less than 3 members
+     * this function will set the MatchRequestStatuses to REMATCH and dissolve the group
+     * 
+     */
+    @Scheduled(cron = "0 0/10 * * * *", zone = "Europe/Berlin")
+    public void groupHealthCheck(){
+
+        // TODO: Implement
+
+    }
 
 }
